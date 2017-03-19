6becee9ad18654ee751a60abacbfa5c9e00d4d8b
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-03-19 01:22:24 +0000

    Add files to project.

---
+++ app.js
@@ -0,0 +1,15 @@
+var request = require('request');
+
+request('https://jsonplaceholder.typicode.com/users/1', function (error, response, body) {
+  console.log('error:', error); // log the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // log the response status code if a response was received
+  console.log('body:', body); // log the body of the request.
+});
+
+/* Comment out all the code above this line and create your own request function
+which uses the Node core HTTP module and it's methods to console.log the same
+values as request function above. */
+
+function request(url){
+  // create your function here
+}
